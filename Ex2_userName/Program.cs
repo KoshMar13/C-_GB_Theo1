9e5f21d52fa410ac310548db262c4a74e6f499e2
Kosh <mail@ex.com>
2022-06-22 19:56:31 +0300

    Ex1...Ex3

---
+++ Ex2_userName/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Insert user name: ");
+string userName = Console.ReadLine();
+Console.Write("Hello, ");
+Console.Write(userName);
+Console.Write("!");
---
+++ Ex3_Summ/Program.cs
@@ -0,0 +1,5 @@
+﻿//Console.Write("Insert 1st number: ");
+int number1 = 3;
+//Console.Write("Insert 2nd number: ");
+int number2 = 5;
+Console.WriteLine(number1 + number2);
