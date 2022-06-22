f4470bbc224be306c7256617ad1e3eff76838955
Kosh <mail@ex.com>
2022-06-22 20:54:39 +0300

    Ex7, Ex8

---
+++ Ex8/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Insert username: ");
+string username = Console.ReadLine();
+int number = new Random().Next(0, 1000);
+Console.Write(username);
+Console.Write(", insert number form 0 to 1000: ");
+int insertedNumber = Console.ReadLine();
